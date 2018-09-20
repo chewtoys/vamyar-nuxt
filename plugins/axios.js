42dcf8286509fb7d5cba9c46c7268239aad0b092
savy <savvyversa@gmail.com>
2018-09-21 00:53:04 +0430

    update package

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,4 @@
-export default function({ $axios, store }) {
+export default function ({$axios, store}) {
   // Authorization
   if (store.state.admin.auth || store.state.user.auth) {
     let accessToken = store.state.user.auth || store.state.admin.auth
@@ -13,10 +13,14 @@ export default function({ $axios, store }) {
   //'Content-Type': 'application/json',
 
   $axios.onRequest(config => {
-    console.log({ 1: "DEBUG ON AXIOS :  Request:", config })
+    console.log({1: "DEBUG ON AXIOS :  Request:", config})
   })
   $axios.onResponse(response => {
     //let { code } = response
     //console.log({2: 'DEBUG ON AXIOS :  onResponse:', 2: response})
+  })
+  $axios.onError(response => {
+    //let { code } = response
+    console.log({2: 'DEBUG ON AXIOS :  onError:', 2: response})
   })
 }
