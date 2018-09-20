fe23d0c4ea78b72db5ac79285e095cb8a7d22a3e
savy <savvyversa@gmail.com>
2018-09-21 01:25:30 +0430

    Add startup script

---
+++ startup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#sudo su -
+#rm -rf vamyar-nuxt 
+#git clone git@github.com:savyir/vamyar-nuxt.git
+
+
+export PATH=/usr/local/bin
+cd /var/www/vamyar.tk/vamyar-nuxt
+
+git status
+git stash
+
+#git pull origin $BRANCH_NAME --force
+git pull origin develop --force
+npm i -g npm@latest
+npm i -g yarn@latest
+npm i -g pm2@latest
+
+export PATH=/opt/rh/rh-nodejs8/root/usr/bin
+pm2 delete all
+export PATH=/usr/local/bin
+yarn
+
+#npm test
+yarn run prod2
+#pm2 start all
+#pm2 start "/usr/local/bin/npm" --name "server" -- start
