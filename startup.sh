58822961ae0f250ccdfe59f9803ffdf4186cb98b
savy <savvyversa@gmail.com>
2018-09-21 15:49:19 +0430

    update startup

--- startup.sh
+++ startup.sh
@@ -6,17 +6,20 @@
 export PATH=/usr/local/bin
 npm i -g npm@latest
 npm i -g yarn@latest
-npm i -g pm2@latest
+#npm i -g pm2@latest
 npm i -g forever@latest
-npm i -g strongloop@latest
+#npm i -g strongloop@latest
 
 export PATH=/usr/local/bin
 yarn
 
+forever stopall || true
 pm2 stop all  || true
 pm2 delete all  || true
+
 yarn build
-forever stopall
-forever start "/usr/local/bin/npm"  start --prefix /var/www/vamyar.tk/vamyar-nuxt
+
+forever start "/usr/local/bin/npm"  start --prefix "/var/www/vamyar.tk/vamyar-nuxt"
 forever list
+
 #pm2 start "/usr/local/bin/npm" --name "server" -- start
