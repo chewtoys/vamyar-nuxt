0240cf23c54345f8bf149111fe0d4fef7c387643
savvyversa <savvyversa@gmail.com>
2018-11-19 03:26:44 +0330

    Update frontpage.vue

--- layouts/frontpage.vue
+++ layouts/frontpage.vue
@@ -7,6 +7,23 @@
 <script>
 import Toolbar from "~/components/site/ToolBar.vue"
 export default {
+  head() {
+    return {
+      title: this.$store.state.navigation.title,
+      titleTemplate: _.get(this.$store.state, 'settings.data.site.siteTitle', '') + '- %s ',
+      meta:
+        [
+          {
+            name: 'keywords',
+            content: _.get(this.$store.state, 'settings.data.site.siteKeywords', '') || " سایت وامیار, vamyar,وام,loan,درخواست ضامن آنلاین,فروش وام , درخواست سرمایه"
+          },
+          {
+            name: 'description',
+            content: _.get(this.$store.state, 'settings.data.site.siteDescription', '') || "سایت وامیار پلتفرم جامع خرید و فروش وام و ضامن و سرمایه"
+          }
+        ]
+    }
+  },
   components: { Toolbar },
   computed: {}
 }
