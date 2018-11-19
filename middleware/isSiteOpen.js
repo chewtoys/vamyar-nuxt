83424a29e1827a5ee850659f860b1ce587fa9a5d
savvyversa <savvyversa@gmail.com>
2018-11-20 00:42:31 +0330

    Update isSiteOpen.js

--- middleware/isSiteOpen.js
+++ middleware/isSiteOpen.js
@@ -1,5 +1,5 @@
 export default function ({store, route, redirect, error}) {
   let status = _.get(store.state, 'settings.data.site.isSiteOpen', true);
   let text = _.get(store.state, 'settings.data.site.closedSiteText', 'سایت در حال بروزرسانی می باشد');
-  if(!status) return error({statusCode: 503 , msg: text})
+  if(!status) return error({statusCode: 503 , message: text})
 }
