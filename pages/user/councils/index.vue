690ea749ffe1a268c54f68ccc0f6bd1bb2f7962a
savvyversa <savvyversa@gmail.com>
2018-12-16 04:01:40 +0330

    Update index.vue

--- pages/user/councils/index.vue
+++ pages/user/councils/index.vue
@@ -21,39 +21,16 @@
         <v-toolbar flat color="white">
           <v-toolbar-title>{{ page_title }}</v-toolbar-title>
           <v-spacer/>
-          <v-btn
-            color="red" outline light round class="mb-2"
-            @click="deleteItems">
-            <v-icon
-              class="mx-1"
-              small
-            >delete
-            </v-icon>
-            حذف انتخاب شده ها
 
-          </v-btn>
           <v-btn :to="uri+`/create`" color="green" outline light round class="mb-2">
             <v-icon class="mx-1" small>create</v-icon>
             درخواست مشاوره
           </v-btn>
         </v-toolbar>
-        <v-card-title>
-          <div v-if="false">
-            جست‌و‌جو
-            <v-spacer></v-spacer>
-            <v-text-field
-              v-model="search"
-              append-icon="search"
-              label="چیزی بنویسید"
-              single-line
-              hide-details
-            ></v-text-field>
-          </div>
-        </v-card-title>
+
         <v-data-table
           v-model="selected"
           item-key="id"
-          select-all
           :headers="headers"
           :items="data"
           :loading="loading"
@@ -75,37 +52,20 @@
             </v-tooltip>
           </template>
           <template slot="items" slot-scope="props">
-            <td>
-              <v-checkbox
-                v-model="props.selected"
-                primary
-                hide-details
-              ></v-checkbox>
+            <td class="text-xs-right">
+              <v-btn color="success" :to="props.item.link">لینک شما: {{ props.item.link }}</v-btn>
             </td>
             <td class="text-xs-right">{{ props.item.title }}</td>
-            <td class="text-xs-right">{{ getCity(props.item) }}</td>
-            <td class="text-xs-right">{{ props.item.job }}</td>
-            <td class="text-xs-right">{{ (props.item.requestType.title) }}</td>
             <td class="text-xs-right">
               <div v-html="nl2br(props.item.requestText)"></div>
             </td>
+            <td class="text-xs-right">{{ getCity(props.item) }}</td>
+            <td class="text-xs-right">{{ props.item.job }}</td>
+
+            <td class="text-xs-right">{{ (props.item.requestType.title) }}</td>
+
             <td class="text-xs-right">{{ props.item.jUpdatedAt }}</td>
-            <td class="text-xs-left">
-              <a title="مشاهده و پاسخ" :href=" uri + '/show/' + props.item.id" class="mx-1">
-                <v-icon
-                  small
-                >
-                  reply
-                </v-icon>
-              </a>
-              <v-icon
-                class="mx-1"
-                small
-                @click="deleteItem(props.item.id)"
-              >
-                delete
-              </v-icon>
-            </td>
+
           </template>
           <v-alert slot="no-results" :value="true" color="error" icon="warning">
             نتیجه ای برای "{{ search }}" یافت نشد.
@@ -128,13 +88,13 @@
     councilRequestTypes = '/councilRequestTypes',
     cityMethod = '/cities?number=3000',
     headers = [
+      {text: 'لینک', value: 'link', align: 'center'},
       {text: 'عنوان', value: 'title', align: 'right'},
+      {text: 'متن درخواست', value: 'requestText', sortable: true, align: 'right'},
       {text: 'شهر', value: 'city', align: 'right'},
       {text: 'شغل', value: 'job', align: 'right'},
       {text: 'نوع', sortable: false, type: 'requestType', align: 'right'},
-      {text: 'متن درخواست', value: 'requestText', sortable: true, align: 'right'},
       {text: 'تاریخ ثبت', value: 'jUpdatedAt', sortable: true, align: 'right'},
-      {text: 'عملیات', sortable: false, align: 'left'},
     ]
 
   export default {
@@ -228,7 +188,7 @@
         return Helper.nl2br(val)
       },
       getCity(item) {
-        return _.get(item, 'city.name','ثبت نشده!');
+        return _.get(item, 'city.name', 'ثبت نشده!');
       },
       requestType(id) {
         let list = this.$store.state.councilTypes.data;
