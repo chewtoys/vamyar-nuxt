46a724a1ad1d7c20b7bcb9b729de0b33d28bc14d
savvyversa <savvyversa@gmail.com>
2019-01-03 07:06:37 +0330

    fix syntax

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,7 +1,14 @@
-export default function ({store, redirect, route}) {
+export default function ({store, $axios, redirect, route}) {
   // If the user is not authenticated
   if (!store.state.user.auth) {
     let path = route.path === '/user' ? '' : route.path;
     return redirect("/user/auth?path=" + encodeURI(path))
+  } else {
+    $axios.$get("/user/subscriptions").then(res => {
+      if (_.has(res, 'data.info')) {
+        store.commit("user/updateUserSubscription", _.get(res, 'data', null))
+      }
+    })
+
   }
 }
--- store/user.js
+++ store/user.js
@@ -1,4 +1,5 @@
 const Cookie = process.client ? require('js-cookie') : undefined
+import Helper from '~/assets/js/helper'
 
 export const state = () => ({
   auth: null,
@@ -20,13 +21,15 @@ export const mutations = {
   },
   updateUserSubscription(state, data) {
     if (_.isArray(data) && data.length > 0) {
+
       state.subscription = data[0];
       state.subscription.period = _.get(data[0], 'period', 0);
-      state.subscription.left =  _.get(data[0], 'info.remainedDays', 0);
+      state.subscription.left = _.get(data[0], 'info.remainedDays', 0);
       state.subscription.endDate = _.get(data[0], 'info.endDate.date', 0);
       state.subscription.expireDate = Helper.dateFormat(_.get(data[0], 'info.endDate.date', ''), 'YYYY/M/D HH:mm:ss', 'jYYYY/jM/jD HH:mm:ss');
       state.subscriptions = data;
       state.hasSubscription = true;
+
     } else {
       state.hasSubscription = false;
     }
